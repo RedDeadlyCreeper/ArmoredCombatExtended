a71b7c4686eae412843c66f760a0c3685346294f
RDC <basilscott@rocketmail.com>
2018-09-05 05:10:27 -0400

    PlaceholdersSwapped

--- lua/acf/shared/guns/atrifle.lua
+++ lua/acf/shared/guns/atrifle.lua
@@ -1,8 +1,8 @@
 --define the class
 ACF_defineGunClass("ATR", {
 	spread = 0.2,
-	name = "Anti Tank Rifle",
-	desc = "Anti Tank rifles fire stupidly fast small bullets to penetrate light armor. I dare you to fire HVAP out of these.",
+	name = "Anti-Tank Rifle",
+	desc = "Anti Tank rifles fire stupidly fast small bullets to penetrate light armor. Built to fire HVAP out of these. Using placeholder models ATM.",
 	muzzleflash = "30mm_muzzleflash_noscale",
 	rofmod = 10,
 	sound = "acf_extra/tankfx/gnomefather/7mm1.wav",
@@ -13,51 +13,51 @@ ACF_defineGunClass("ATR", {
 --add a gun to the class
 ACF_defineGun("7.92mmATR", { --id
 	name = "(+)7.92mm Anti Tank Rifle",
-	desc = "The 7.92 Anti Tank Rifle is the desperate attempt of someone to fend off hordes of tracked beasts in the trenches",
+	desc = "The 7.92 Anti Tank Rifle is somone's desperate attempt to fend off hordes of tracked beasts in the trenches",
 	model = "models/tankgun/atrifle_792mm.mdl",
 	gunclass = "ATR",
 	caliber = 0.792,
 	weight = 90,
 	year = 1917,
-	rofmod = 1,
+	rofmod = 1.2,
 	magsize = 5,
 	magreload = 6,
 	round = {
-		maxlength = 11,
-		propweight = 1.6
+		maxlength = 12,
+		propweight = 1.7
 	}
 } )
 
 ACF_defineGun("14.5mmATR", { --id
 	name = "(+)14.5mm Anti Tank Rifle",
-	desc = "Commonly used by soviets as a budget way to kill expensive stuff, still worthless.",
+	desc = "Commonly used by soviets as a budget way to kill tanks, still worthless.",
 	model = "models/tankgun/atrifle_145mm.mdl",
 	gunclass = "ATR",
 	caliber = 1.45,
 	weight = 130,
 	year = 1917,
-	rofmod = 1,
+	rofmod = 1.1,
 	magsize = 5,
 	magreload = 8,
 	round = {
-		maxlength = 19,
-		propweight = 2.9
+		maxlength = 19.5,
+		propweight = 3.2
 	}
 } )
 
 ACF_defineGun("20mmATR", { --id
 	name = "(+)20mm Anti Tank Rifle",
 	desc = "Collosal anti tank rifle, good for putting a hole through side armor at point blank, that is if you can carry it.",
-	model = "models/tankgun/atrifle_20mm.mdl",
+	model = "models/machinegun/machinegun_20mm.mdl",
 	gunclass = "ATR",
 	caliber = 2,
 	weight = 190,
 	year = 1917,
-	rofmod = 1,
+	rofmod = 0.9,
 	magsize = 5,
 	magreload = 10,
 	round = {
-		maxlength = 21,
-		propweight = 4.5
+		maxlength = 22,
+		propweight = 5.1
 	}
 } )
--- lua/acf/shared/guns/grenadelauncher.lua
+++ lua/acf/shared/guns/grenadelauncher.lua
@@ -29,8 +29,8 @@ ACF_defineGun("40mmGL", { --id
 
 ACF_defineGun("20mmGL", { --id
 	name = "(+)20mm Grenade Launcher",
-	desc = "The 20mm is the embodyment of wimpy weapons, although it has a large clip and can fire HE it is bloody weak",
-	model = "models/launcher/20mmgl.mdl",
+	desc = "The 20mm is the embodyment of wimpy weapons, although it has a large clip and can fire HE it is bloody weak                using 40mm GL as placeholder bc the 20mm mini is borked",
+	model = "models/launcher/40mmgl.mdl",
 	gunclass = "GL",
 	canparent = true,
 	caliber = 2.0,
