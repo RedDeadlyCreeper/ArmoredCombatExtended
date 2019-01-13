9825ff77ca296ab24e286dbafade67808c091f22
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-01-12 22:15:56 -0500

    Update smokelauncher.lua

--- lua/acf/shared/guns/smokelauncher.lua
+++ lua/acf/shared/guns/smokelauncher.lua
@@ -1,6 +1,6 @@
 --define the class
 ACF_defineGunClass("SL", {
-	spread = 0.32,
+	spread = 0.4,
 	name = "Smoke Launcher",
 	desc = "Smoke launcher to block an attacker's line of sight.",
 	muzzleflash = "40mm_muzzleflash_noscale",
@@ -18,7 +18,7 @@ ACF_defineGun("40mmSL", { --id
 	gunclass = "SL",
 	canparent = true,
 	caliber = 4.0,
-	rofmod = 0.6,
+	rofmod = 1.2,
 	weight = 1,
 	year = 1941,
 	round = {
@@ -36,24 +36,6 @@ ACF_defineGun("40mmCL", { --id
 	caliber = 4.0,
 	weight = 20,
 	rofmod = 0.015,
-	magsize = 6,
-	magreload = 40,
-	year = 1950,
-	round = {
-		maxlength = 12,
-		propweight = 0.001
-	}
-} )
-
-ACF_defineGun("40mmSLMulti", { --id
-	name = "(+)40mm MultiBarrel Smoke Launcher",
-	desc = "Parentable Multibarrel Smoke Launcher",
-	model = "models/launcher/40mmgl.mdl",
-	gunclass = "SL",
-	canparent = true,
-	caliber = 4.0,
-	weight = 35,
-	rofmod = 0.025,
 	magsize = 6,
 	magreload = 20,
 	year = 1975,
