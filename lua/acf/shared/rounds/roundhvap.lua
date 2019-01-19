76d4feb5b8907cec6995d7c5248e87e5632c1e53
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-01-19 03:48:49 -0500

    Updated Model

--- lua/acf/shared/rounds/roundhvap.lua
+++ lua/acf/shared/rounds/roundhvap.lua
@@ -7,7 +7,7 @@ local Round = {}
 
 Round.type = "Ammo" --Tells the spawn menu what entity to spawn
 Round.name = "High-Velocity Armor Piercing (HVAP)" --Human readable name
-Round.model = "models/munitions/dart_100mm.mdl" --Shell flight model
+Round.model = "models/munitions/round_100mm_ap_shot.mdl" --Shell flight model
 Round.desc = "A soft projectile that contains a heavy tungsten core, penetrates and does a lot more damage than APDS or APFSDS, but has horrible drag characteristics"
 Round.netid = 11 --Unique ammotype ID for network transmission
 
