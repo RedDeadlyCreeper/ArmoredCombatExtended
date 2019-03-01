ed8acf478567fe3929cf555926d571d8b2f5e079
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-02-28 22:35:25 -0500

    Nothing's HE Re-definitions

--- lua/acf/shared/rounds/roundhe.lua
+++ lua/acf/shared/rounds/roundhe.lua
@@ -132,7 +132,7 @@ end
 
 function Round.endflight( Index, Bullet, HitPos, HitNormal )
 	
-	ACF_HE( HitPos - Bullet.Flight:GetNormalized()*3 , HitNormal , Bullet.FillerMass , Bullet.ProjMass - Bullet.FillerMass , Bullet.Owner )
+	ACF_HE( HitPos - Bullet.Flight:GetNormalized()*3, HitNormal, Bullet.FillerMass, Bullet.ProjMass - Bullet.FillerMass, Bullet.Owner, nil, Bullet.Gun )
 	ACF_RemoveBullet( Index )
 	
 end
