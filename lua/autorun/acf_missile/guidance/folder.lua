5bdb065f1dd4b401dba41e36d9102130b8902711
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-03-14 19:57:14 -0400

    Added IR Guidance

--- lua/autorun/acf_missile/guidance/folder.lua
+++ lua/autorun/acf_missile/guidance/folder.lua
@@ -10,5 +10,8 @@ include("autorun/acf_missile/guidance/guidance_laser.lua")
 AddCSLuaFile("autorun/acf_missile/guidance/guidance_radar.lua")
 include("autorun/acf_missile/guidance/guidance_radar.lua")
 
+AddCSLuaFile("autorun/acf_missile/guidance/guidance_infared.lua")
+include("autorun/acf_missile/guidance/guidance_infared.lua")
+
 AddCSLuaFile("autorun/acf_missile/guidance/guidance_antimissile.lua")
 include("autorun/acf_missile/guidance/guidance_antimissile.lua")
