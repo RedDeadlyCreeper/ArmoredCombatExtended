9cfc0f7ad357954974ede3578757eae647635be1
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-03-19 02:14:49 -0400

    Added The Behemoth

--- lua/acf/shared/guns/howitzer.lua
+++ lua/acf/shared/guns/howitzer.lua
@@ -98,29 +98,29 @@ ACF_defineGun("240mmHW", {
 
 ACF_defineGun("290mmHW", {
 	name = "(+)290mm Howitzer",
-	desc = " Mother of all howitzers. This 12in beast can be found on battleships. It WILL fuck your day up... when it reloads.",
+	desc = " Mother of all howitzers. This 12in beast was used to shell absurd underground compound. Using it is truly a warcrime.",
 	model = "models/howitzer/howitzer_290mm.mdl",
 	gunclass = "HW",
 	caliber = 29,
 	weight = 24960,
 	year = 1900,
 	round = {
-		maxlength = 325,
+		maxlength = 360,
 		propweight = 57.0
 	}
 } )
-/*
+
 ACF_defineGun("406mmHW", {
 	name = "(+)406mm Howitzer",
-	desc = "The ultimate anti evreything weapon, this absurd 16 inch gun can commonly be found on American Battleships and emplacements and its usage in acf is a litteral war crime. HOW COULD YOU YOU MONSTER!",
+	desc = "The ultimate anti evreything weapon, this absurd 16 inch gun can commonly be found on American Battleships and emplacements and its mere existence is a warcrime. HOW COULD YOU YOU MONSTER!",
 	model = "models/howitzer/howitzer_406mm.mdl",
 	gunclass = "HW",
 	caliber = 40.6,
 	weight = 42520,
 	year = 1920,
 	round = {
-		maxlength = 520,
+		maxlength = 580,
 		propweight = 98.3
 	}
 } )
-*/
+
