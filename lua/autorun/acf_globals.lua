808b2f8922d3833b6a5089eaa0fe7dac0b456beb
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2019-11-09 00:32:35 -0500

    oof

--- lua/autorun/acf_globals.lua
+++ lua/autorun/acf_globals.lua
@@ -2,7 +2,7 @@ ACF = {}
 ACF.AmmoTypes = {}
 ACF.MenuFunc = {}
 ACF.AmmoBlacklist = {}
-ACF.Version = 418  -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex   Update the changelog too! -Ferv
+ACF.Version = 420  -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex   Update the changelog too! -Ferv
 ACF.CurrentVersion = 0 -- just defining a variable, do not change
 
 ACF.Year = 2019
