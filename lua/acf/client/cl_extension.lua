2dc9da9843c0ab2e4d8287a7718151049562838f
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-06-27 22:44:15 +0200

    refactor: use `HasGodMode`

--- lua/acf/client/cl_extension.lua
+++ lua/acf/client/cl_extension.lua
@@ -257,7 +257,7 @@ function ACEE_SBlast( HitPos, Radius, HitWater, HitWorld )
 					end
 
 					--Tinnitus function
-					if ACE.EnableTinnitus and not ply.ACE_HasGodMode then
+					if ACE.EnableTinnitus and not ply:HasGodMode() then
 						local TinZone = math.max(Radius * 80, 50) * ACE.TinnitusZoneMultipler
 
 						if Dist <= TinZone and ACE_SHasLOS(HitPos) and entply == ply and not ply.aceposoverride then
