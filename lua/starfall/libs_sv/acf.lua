8ee1d46984aa097881b29cb7a8d40e617298d7a7
Matt <rcheeze1337@gmail.com>
2023-11-11 00:36:32 -0500

    Add acf.get* list functions, remove "validPhysics" func
    Don't think the validPhysics function was actually necessary

--- lua/starfall/libs_sv/acf.lua
+++ lua/starfall/libs_sv/acf.lua
@@ -9,6 +9,7 @@ local min, max, clamp, abs, round = math.min, math.max, math.Clamp, math.abs, ma
 local rad, cos = math.rad, math.cos
 
 local checkluatype = SF.CheckLuaType
+local checkpermission = SF.Permissions.check
 local registerprivilege = SF.Permissions.registerPrivilege
 
 registerprivilege("acf.createMobility", "Create acf engine", "Allows the user to create ACF engines and gearboxes", { usergroups = { default = 3 } })
@@ -17,45 +18,23 @@ registerprivilege("acf.createGun", "Create acf gun", "Allows the user to create 
 registerprivilege("acf.createAmmo", "Create acf ammo", "Allows the user to create ACF ammoboxes", { usergroups = { default = 3 } } )
 registerprivilege("entities.acf", "ACF", "Allows the user to control ACF components", { entities = {} })
 
--- Borrowed from https://github.com/wiremod/wire/blob/master/lua/entities/gmod_wire_expression2/core/e2lib.lua#L188
-local function validPhysics(ent)
-	if IsValid(ent) then
-		if ent:IsWorld() then return false end
-		if ent:GetMoveType() ~= MOVETYPE_VPHYSICS then return false end
-
-		return IsValid(ent:GetPhysicsObject())
-	end
-
-	return false
-end
-
 local function isEngine(ent)
-	if not validPhysics( ent ) then return false end
-
 	return ent:GetClass() == "acf_engine"
 end
 
 local function isGearbox(ent)
-	if not validPhysics(ent) then return false end
-
 	return ent:GetClass() == "acf_gearbox"
 end
 
 local function isGun(ent)
-	if not validPhysics(ent) then return false end
-
 	return ent:GetClass() == "acf_gun"
 end
 
 local function isAmmo(ent)
-	if not validPhysics(ent) then return false end
-
 	return ent:GetClass() == "acf_ammo"
 end
 
 local function isFuel(ent)
-	if not validPhysics(ent) then return false end
-
 	return ent:GetClass() == "acf_fueltank"
 end
 
@@ -66,12 +45,58 @@ local radarTypes = {
 }
 
 local function isRadar(ent)
-	if not validPhysics(ent) then return false end
-
 	return radarTypes[ent:GetClass()] or false
 end
 
+-- link resources within each ent type. should point to an ent: true if adding link.Ent, false to add link itself
+local linkTables = {
+	acf_engine		= { GearLink = true, FuelLink = false },
+	acf_gearbox		= { WheelLink = true, Master = false },
+	acf_fueltank	= { Master = false },
+	acf_gun			= { AmmoLink = false },
+	acf_ammo		= { Master = false }
+}
+
+local function getLinks(ent, enttype)
+	local ret = {}
+	-- find the link resources available for this ent type
+	for entry, mode in pairs(linkTables[enttype]) do
+		if not ent[entry] then
+			SF.Throw("[Internal ACE Error] Couldn't find link resource " .. entry .. " for entity " .. tostring(ent), 2)
+
+			return
+		end
+
+		-- find all the links inside the resources
+		for _, link in pairs(ent[entry]) do
+			ret[#ret + 1] = mode and link.Ent or link
+		end
+	end
+
+	return ret
+end
+
+local function searchForGearboxLinks(ent)
+	local boxes = ents.FindByClass("acf_gearbox")
+	local ret = {}
+	for _, box in ipairs(boxes) do
+		if IsValid(box) then
+			for _, link in pairs(box.WheelLink) do
+				if link.Ent == ent then
+					ret[#ret + 1] = box
+
+					break
+				end
+			end
+		end
+	end
+
+	return ret
+end
+
+
 return function(instance)
+
 
 local checktype = instance.CheckType
 local acf_library = instance.Libraries.acf
@@ -79,6 +104,7 @@ local ents_methods, ewrap, eunwrap = instance.Types.Entity.Methods, instance.Typ
 local vec_meta, vwrap, vunwrap = instance.Types.Vector, instance.Types.Vector.Wrap, instance.Types.Vector.Unwrap
 local ang_meta, awrap, aunwrap = instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
 local owrap, ounwrap = instance.WrapObject, instance.UnwrapObject
+local sanitize = instance.Sanitize
 local getent = instance.Types.Entity.GetEntity
 
 local function restrictInfo(ent)
@@ -87,6 +113,21 @@ local function restrictInfo(ent)
 	end
 
 	return false
+end
+
+local idNameCache = {}
+local function listIDFromName(list, name)
+	idNameCache[list] = idNameCache[list] or {}
+
+	if idNameCache[list][name] then return idNameCache[list][name] end
+
+	for id, data in pairs(list) do
+		if data.name == name then
+			idNameCache[list][name] = id
+
+			return id
+		end
+	end
 end
 
 instance:AddHook("deinitialize", function()
@@ -157,6 +198,119 @@ do
 
 		return ""
 	end
+
+	--- Returns the maximum capacity of an acf ammo crate or fuel tank
+	-- @server
+	-- @return number The capacity
+	function ents_methods:acfCapacity()
+		local this = getent(self)
+
+		if not (isAmmo(this) or isFuel(this)) then return 0 end
+		if restrictInfo(this) then return 0 end
+
+		return this.Capacity or 1
+	end
+
+	--- Returns true if the acf engine, fuel tank, or ammo crate is active
+	-- @server
+	-- @return boolean Is the entity active?
+	function ents_methods:acfGetActive()
+		local this = getent(self)
+
+		if not (isEngine(this) or isAmmo(this) or isFuel(this)) then return false end
+		if restrictInfo(this) then return false end
+		if not isAmmo(this) then
+			if this.Active then return true end
+		else
+			if this.Load then return true end
+		end
+
+		return false
+	end
+
+	--- Turns an ACF engine, ammo crate, or fuel tank on or off
+	-- @server
+	-- @param boolean state The state to set the entity to
+	function ents_methods:acfSetActive(on)
+		local this = getent(self)
+
+		checkpermission(instance, this, "entities.acf")
+
+		if not (isEngine(this) or isAmmo(this) or isFuel(this)) then return end
+
+		this:TriggerInput("Active", on and 1 or 0)
+	end
+
+	--- Returns true if hitpos is on a clipped part of prop
+	-- @server
+	-- @param Vector hitpos The hit position
+	-- @return boolean Is the hit position on a clipped part of the prop?
+	function ents_methods:acfHitClip(hitpos)
+		checktype(hitpos, vec_meta)
+
+		local this = getent(self)
+		hitpos = vunwrap(hitpos)
+
+		checkpermission(instance, this, "entities.acf")
+		if ACF_CheckClips(nil, nil, this, hitpos) then
+			return true
+		else
+			return false
+		end
+	end
+
+	--- Returns the ACF links associated with the entity
+	-- @server
+	-- @return table The links
+	function ents_methods:acfLinks()
+		local this = getent(self)
+
+		local enttype = this:GetClass()
+		if not linkTables[enttype] then return searchForGearboxLinks(this) end
+
+		return sanitize(getLinks(this, enttype))
+	end
+
+	--- Returns the full name of an ACF entity
+	-- @server
+	-- @return string The full name
+	function ents_methods:acfName()
+		local this = getent(self)
+
+		if isAmmo(this) then return this.RoundId .. " " .. this.RoundType end
+		if isFuel(this) then return this.FuelType .. " " .. this.SizeId end
+
+		local acftype = ""
+
+		if isEngine(this) then acftype = "Mobility" end
+		if isGearbox(this) then acftype = "Mobility" end
+		if isGun(this) then acftype = "Guns" end
+
+		if acftype == "" then return "" end
+		local List = ACF.Weapons
+
+		return List[acftype][this.Id].name or ""
+	end
+
+	--- Returns the type of ACF entity
+	-- @server
+	-- @return string The type
+	function ents_methods:acfType()
+		local this = getent(self)
+
+		if isEngine(this) or isGearbox(this) then
+			return ACF.Weapons["Mobility"][this.Id].category or ""
+		end
+
+		if isGun(this) then
+			return ACF.Classes["GunClass"][this.Class].name or ""
+		end
+
+		if isAmmo(this) then return this.RoundType or "" end
+		if isFuel(this) then return this.FuelType or "" end
+
+		return ""
+	end
 end
 
 -- Spawning functions
@@ -178,8 +332,136 @@ do
 	end
 end
 
+-- Weapon functions
+do
+	--- Returns the specs of gun
+	-- @param string id id or name of the gun
+	-- @server
+	-- @return table The specs table
+	function acf_library.getGunSpecs(id)
+		checkluatype(id, TYPE_STRING)
+
+		local listEntries = ACF.Weapons.Guns
+
+		-- Not a valid id, try name
+		if not listEntries[id] then
+			id = listIDFromName(listEntries, id)
+
+			-- Name is also invalid, error
+			if not id or not listEntries[id] then
+				SF.Throw("Invalid id or name", 2)
+			end
+		end
+
+		local specs = table.Copy(listEntries[id])
+		specs.BaseClass = nil
+
+		return sanitize(specs)
+	end
+
+	--- Returns a list of all guns
+	-- @server
+	-- @return table The guns list
+	function acf_library.getAllGuns()
+		local tbl = {}
+
+		for id, _ in pairs(ACF.Weapons.Guns) do
+			tbl[#tbl + 1] = id
+		end
+
+		return tbl
+	end
+end
+
+-- Ammo functions
+do
+end
+
 -- Mobility functions
 do
+	--- Returns the specs of an engine or gearbox
+	-- @param string id ID or name of the engine or gearbox
+	-- @server
+	-- @return table The specs table
+	function acf_library.getMobilitySpecs(id)
+		checkluatype(id, TYPE_STRING)
+
+		local listEntries = ACF.Weapons.Mobility
+
+		-- Not a valid id, try name
+		if not listEntries[id] then
+			id = listIDFromName(listEntries, id)
+
+			-- Name is also invalid, error
+			if not id or not listEntries[id] then
+				SF.Throw("Invalid id or name", 2)
+			end
+		end
+
+		local specs = table.Copy(listEntries[id])
+		specs.BaseClass = nil
+
+		return sanitize(specs)
+	end
+
+	--- Returns a list of all mobility components
+	-- @server
+	-- @return table The mobility component list
+	function acf_library.getAllMobility()
+		local tbl = {}
+
+		for id, _ in pairs(ACF.Weapons.Mobility) do
+			tbl[#tbl + 1] = id
+		end
+
+		return tbl
+	end
+
+	--- Returns a list of all engines
+	-- @server
+	-- @return table The engine list
+	function acf_library.getAllEngines()
+		local tbl = {}
+
+		for id, d in pairs(ACF.Weapons.Mobility) do
+			if d.ent == "acf_engine" then
+				tbl[#tbl + 1] = id
+			end
+		end
+
+		return tbl
+	end
+
+	--- Returns a list of all gearboxes
+	-- @server
+	-- @return table The gearbox list
+	function acf_library.getAllGearboxes()
+		local tbl = {}
+
+		for id, d in pairs(ACF.Weapons.Mobility) do
+			if d.ent == "acf_gearbox" then
+				tbl[#tbl + 1] = id
+			end
+		end
+
+		return tbl
+	end
+
+	--- Returns the specs of the fuel tank
+	-- @param string id id of the engine or gearbox
+	-- @server
+	-- @return table The specs table
+	function acf_library.getFuelTankSpecs(id)
+		checkluatype(id, TYPE_STRING)
+
+		local list_entries = ACF.Weapons.FuelTanks
+		if not list_entries[id] then SF.Throw("Invalid id", 2) end
+
+		local specs = table.Copy(list_entries[id])
+		specs.BaseClass = nil
+
+		return sanitize(specs)
+	end
 end
 
 end
