2c7535b3b25d75d4b40ade9930bfdc9e390ec1a7
RDC <basilscott@rocketmail.com>
2025-01-26 05:00:24 -0500

    Increased damage on nonpens by removing exponent

--- lua/acf/shared/armor/du.lua
+++ lua/acf/shared/armor/du.lua
@@ -113,7 +113,7 @@ if SERVER then
 		-- Projectile did not breach nor penetrate armor
 		local Penetration = math.min( maxPenetration , losArmor * effectiveness )
 
-		HitRes.Damage	= ( Penetration / losArmorHealth / effectiveness ) ^ 2 * FrArea * resiliance * damageMult * ductilitymult
+		HitRes.Damage	= ( Penetration / losArmorHealth / effectiveness ) * FrArea * resiliance * damageMult * ductilitymult
 		HitRes.Overkill = 0
 		HitRes.Loss	= 1
 
